59d81054c88921b7144436bee40f09de566660d8
Liat Berkovich <lberkovi@redhat.com>
2026-05-17 00:36:17 +0300

    Update SystemResourcesContent.tsx

--- libs/ui-components/src/components/Device/DeviceDetails/DeviceDetailsTabContent/SystemResourcesContent.tsx
+++ libs/ui-components/src/components/Device/DeviceDetails/DeviceDetailsTabContent/SystemResourcesContent.tsx
@@ -25,6 +25,7 @@ const SystemResourcesContent = ({ device }: { device: Required<Device> }) => {
             <DescriptionListTerm>{t('CPU pressure')}</DescriptionListTerm>
             <DescriptionListDescription>
               <DeviceResourceStatus device={device} monitorType={MonitorType.cpu} />
+              <span>la laaaaala</span>
             </DescriptionListDescription>
           </DescriptionListGroup>
           <DescriptionListGroup>
