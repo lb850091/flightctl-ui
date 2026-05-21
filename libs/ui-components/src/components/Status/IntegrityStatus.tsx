22cd18f1a6de47c362e41a0f0d75203e06417770
lb850091 <134092295+lb850091@users.noreply.github.com>
2026-05-22 00:16:42 +0300

    Update IntegrityStatus.tsx

--- libs/ui-components/src/components/Status/IntegrityStatus.tsx
+++ libs/ui-components/src/components/Status/IntegrityStatus.tsx
@@ -97,7 +97,7 @@ const IntegrityStatus = ({ integrityStatus }: { integrityStatus?: DeviceIntegrit
         <StackItem>{extraDetails}</StackItem>
         {integrityStatus.lastVerified && (
           <StackItem>
-            <div>ddd</div>
+            <div>dd1111d</div>
             <small>
               {t('Last verification at: {{ timestamp }}', {
                 timestamp: getDateDisplay(integrityStatus.lastVerified),
