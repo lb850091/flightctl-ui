13958c2de4915c2b925091e71b1493044526d7f2
lb850091 <134092295+lb850091@users.noreply.github.com>
2026-05-24 01:07:12 +0300

    Update DetailsPageCard.tsx

--- libs/ui-components/src/components/DetailsPage/DetailsPageCard.tsx
+++ libs/ui-components/src/components/DetailsPage/DetailsPageCard.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 
 
 
-
-
 import { Card, CardProps } from '@patternfly/react-core';
 
 const DetailsPageCard = (props: CardProps) => <Card isFullHeight {...props} ref={undefined} />;
