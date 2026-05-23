c2a8519ce0573c129a0bc3295203fd63ed24e9cf
lb850091 <134092295+lb850091@users.noreply.github.com>
2026-05-24 01:11:30 +0300

    Update DetailsPageCard.tsx

--- libs/ui-components/src/components/DetailsPage/DetailsPageCard.tsx
+++ libs/ui-components/src/components/DetailsPage/DetailsPageCard.tsx
@@ -4,4 +4,7 @@ import { Card, CardProps } from '@patternfly/react-core';
 const DetailsPageCard: React.FC<CardProps> = (props) => <Card style={{ height: '100%' }} {...props} ref={undefined} />;
 
 
+
+
+
 export default DetailsPageCard;
